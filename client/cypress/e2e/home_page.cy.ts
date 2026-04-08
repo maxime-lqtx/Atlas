f218a28cf1d2a1b3b3bcd0436d75b144d95b7fd3
maxime <maximelarquetoux@gmail.com>
2026-04-08 11:43:03 +0200

    test 2e2 for homepage

---
+++ client/cypress/e2e/home_page.cy.ts
@@ -0,0 +1,5 @@
+describe('The Home Page', ()=>{
+    it('should successfully loads', ()=>{
+        cy.visit('/')
+    })
+})
--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -1,17 +1,17 @@
-describe('My First Test', () => {
+describe("My First Test", () => {
   it('finds the content "type"', () => {
-// visit le'url
-    cy.visit('https://example.cypress.io')
+    // visit le'url
+    cy.visit("https://example.cypress.io");
 
     // click sur type
-    cy.contains('type').click()
+    cy.contains("type").click();
 
     // check l'url
-    cy.url().should('include', '/commands/actions')
+    cy.url().should("include", "/commands/actions");
     // get an input, écris dedans
-    cy.get('.action-email').type('fake@email.com')
+    cy.get(".action-email").type("fake@email.com");
 
     // verify that the value has been updated
-    cy.get('.action-email').should('have.value', 'fake@email.com')
-  })
-})
+    cy.get(".action-email").should("have.value", "fake@email.com");
+  });
+});
