b97c569cc9326fee4ccca7b71b9d17a0eef83b50
maxime <maximelarquetoux@gmail.com>
2026-04-08 11:22:25 +0200

    tests examples

---
+++ client/cypress/e2e/example.cy.ts
@@ -0,0 +1,14 @@
+
+describe('should visit a site + actions',()=>{
+    it('should success',()=>{
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type').click()
+
+        cy.url().should('include', '/commands/actions')
+
+        cy.get('.action-email').type('fake@email.com')
+
+        cy.get('.action-email').should('have.value', "fake@email.com")
+    })
+})
